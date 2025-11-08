3c1e8eaeeba850430633113634fb3f5d0d472b23
Victor Couto <coutocidadevictor@gmail.com>
2025-11-08 15:29:25 -0300

    Commit inicial do projeto

---
+++ src/main/java/com/exemplo/projeto/perfil_web/PerfilWebApplication.java
@@ -0,0 +1,13 @@
+package com.exemplo.projeto.perfil_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfilWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfilWebApplication.class, args);
+	}
+
+}
